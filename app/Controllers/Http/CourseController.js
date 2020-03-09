9e7393da26cee4c1c83ef54147b9a91294c3e501
krthr <twilson@uninorte.edu.co>
2020-03-09 13:11:36 -0500

    Professor and Course controllers

---
+++ app/Controllers/Http/CourseController.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Course = use("App/Models/Course");
+
+/**
+ * Resourceful controller for interacting with courses
+ */
+class CourseController {
+  /**
+   * Show a list of all courses.
+   * GET courses
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {}
+
+  /**
+   * Create/save a new course.
+   * POST courses
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {}
+
+  /**
+   * Display a single course.
+   * GET courses/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+}
+
+module.exports = CourseController;
---
+++ app/Controllers/Http/ProfessorController.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with professors
+ */
+class ProfessorController {
+  /**
+   * Show a list of all professors.
+   * GET professors
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {}
+
+  /**
+   * Create/save a new professor.
+   * POST professors
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {}
+
+  /**
+   * Display a single professor.
+   * GET professors/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+}
+
+module.exports = ProfessorController;
