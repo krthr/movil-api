b624240d2ac0284031d8430a6b3e73e1fb87faa3
krthr <twilson@uninorte.edu.co>
2020-03-09 19:34:53 -0500

    Code re-format

--- app/Controllers/Http/CourseController.js
+++ app/Controllers/Http/CourseController.js
@@ -2,7 +2,6 @@
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
 
 const NotFoundException = use("App/Exceptions/NotFoundException");
 const Course = use("App/Models/Course");
--- app/Controllers/Http/ProfessorController.js
+++ app/Controllers/Http/ProfessorController.js
@@ -2,7 +2,6 @@
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
 
 const NotFoundException = use("App/Exceptions/NotFoundException");
 const Professor = use("App/Models/Professor");
