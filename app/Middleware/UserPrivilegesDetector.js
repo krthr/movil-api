30f44b54dcc1e89464c3d4c6145a4d6d80f129d2
krthr <twilson@uninorte.edu.co>
2020-04-17 16:23:11 -0500

    username on UserPriviligesDetector

--- app/Middleware/UserPrivilegesDetector.js
+++ app/Middleware/UserPrivilegesDetector.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
@@ -9,21 +9,20 @@ class UserPrivilegesDetector {
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ request, params, response, auth }, next) {
+  async handle({ request, params, response, auth }, next) {
     try {
       const { dbId } = params;
-      var userJwt = await auth.getUser()
-      if( (userJwt.dbprefix).toString() == dbId ){
-        await next()
-      }else{
-        response.send('No Privileges To Access This Information')
+      var userJwt = await auth.getUser();
+      if (userJwt.username.toString() == dbId) {
+        await next();
+      } else {
+        response.send("No Privileges To Access This Information");
       }
     } catch (error) {
-      console.log(error)
-      response.send('Credentials missing')
+      console.log(error);
+      response.send("Credentials missing");
     }
-
   }
 }
 
-module.exports = UserPrivilegesDetector
+module.exports = UserPrivilegesDetector;
