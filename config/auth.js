7d1e70908d842e6148ea4c2833fe95b12db17133
krthr <twilson@uninorte.edu.co>
2020-05-12 18:35:17 -0500

    TOKEN_EXPIRES_IN

--- config/auth.js
+++ config/auth.js
@@ -73,7 +73,7 @@ module.exports = {
     password: "password",
     options: {
       secret: Env.get("APP_KEY"),
-      expiresIn: "40m",
+      expiresIn: Env.get("TOKEN_EXPIRES_IN", "2m"),
     },
   },
 
